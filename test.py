379a5cb764b7ddf6d29964f41d39016a1345c695
Fábio Albuquerque <fabio_albuquerque2@hotmail.com>
2026-03-09 00:02:23 +0000

    File rename and configs

---
+++ test.py
@@ -0,0 +1,7 @@
+# from pyspark.sql import SparkSession
+# spark = SparkSession.builder.getOrCreate()
+
+print('test1')
+# spark.sql("select 1").display()
+print('test2')
+
--- test2.py
+++
@@ -1,4 +0,0 @@
-print('test1')
-spark.sql("select 1").display()
-print('test2')
-
